98c6aba929040f969af1db828a6a1653f93037d0
Sean Fitzgerald <sean@fitzgeralds.me>
2024-03-15 20:24:21 -0700

    formatting

--- client.py
+++ client.py
@@ -9,7 +9,8 @@ import requests
 from bs4 import BeautifulSoup
 from heare.config import SettingsDefinition, Setting
 
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0" \
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) " \
+             "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0" \
              " Safari/537.36"
 
 logging.basicConfig(
